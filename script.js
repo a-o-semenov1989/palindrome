b077b4131e0512f8034960127d0a5116a9eddcd0
Anatolii Semenov <a-o-semenov1989@gmail.com>
2020-08-07 11:43:44 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const palindrome = (str) => {
+  // Change str to lower case
+  str = str.toLowerCase();
+  // String to array, than reverse the string and return result of equality
+  return str === str.split('').reverse().join('');
+}
+
+const isPalindrome = palindrome('Kayak');
+
+console.log(isPalindrome);
